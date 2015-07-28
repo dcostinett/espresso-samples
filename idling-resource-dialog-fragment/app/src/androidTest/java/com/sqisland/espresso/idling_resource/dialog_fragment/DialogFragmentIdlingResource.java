ca1af5f0c8b29b22fb4a6e318523b0194a9f0467
Dan Costinett <daniel.r.costinett.-nd@disney.com>
2015-07-28 16:20:12 -0700

    clean-up, assign callback

--- idling-resource-dialog-fragment/app/src/androidTest/java/com/sqisland/espresso/idling_resource/dialog_fragment/DialogFragmentIdlingResource.java
+++ idling-resource-dialog-fragment/app/src/androidTest/java/com/sqisland/espresso/idling_resource/dialog_fragment/DialogFragmentIdlingResource.java
@@ -1,11 +1,8 @@
 /* (c) Disney. All rights reserved. */
 package com.sqisland.espresso.idling_resource.dialog_fragment;
 
-import android.hardware.input.InputManager;
 import android.support.test.espresso.IdlingResource;
 import android.support.v4.app.FragmentManager;
-
-import javax.net.ssl.ManagerFactoryParameters;
 
 /**
  * @author costd037 on 7/28/15.
@@ -23,7 +20,7 @@ public class DialogFragmentIdlingResource implements IdlingResource {
 
     @Override
     public String getName() {
-        return DialogFragmentIdlingResource.class.getSimpleName();
+        return DialogFragmentIdlingResource.class.getSimpleName() + ":" + tag;
     }
 
     @Override
@@ -37,6 +34,6 @@ public class DialogFragmentIdlingResource implements IdlingResource {
 
     @Override
     public void registerIdleTransitionCallback(ResourceCallback resourceCallback) {
-
+        this.callback = resourceCallback;
     }
 }
